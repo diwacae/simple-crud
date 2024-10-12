f21a5a5c656b7af3b74f0fd0ba913870371b8eb7
angganurbayu <bayunurangga21@gmail.com>
2024-10-12 21:10:46 +0700

    update index and adding css style link

--- index.php
+++ index.php
@@ -1,48 +1,53 @@
- <?php
-  include "config/connection.php";
+<?php
+include "config/connection.php";
 
-  $sql = "SELECT * FROM mahasiswa";
-  $result = $mysqli->query($sql);
+$sql = "SELECT * FROM mahasiswa";
+$result = $mysqli->query($sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-  ?>
- <!DOCTYPE html>
- <html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Data Mahasiswa</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
 
- <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Data Mahasiswa</title>
- </head>
+<body>
+  <h1>Data Mahasiswa</h1>
+  <a href="add.php" class="btn-add">Tambah Data</a>
 
- <body>
-   <h1>Data Mahasiswa</h1>
-   <a href="add.php">Tambah Data</a>
-   <table border="1">
-     <tr>
-       <th>No</th>
-       <th>NIM</th>
-       <th>Nama</th>
-       <th>Program Studi</th>
-       <th>Semester</th>
-       <th>Aksi</th>
-     </tr>
-     <?php
+  <table>
+    <thead>
+      <tr>
+        <th>No</th>
+        <th>NIM</th>
+        <th>Nama</th>
+        <th>Program Studi</th>
+        <th>Semester</th>
+        <th>Aksi</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
       $no = 1;
       while ($row = $result->fetch_assoc()):
       ?>
-       <tr>
-         <td><?= $no++; ?></td>
-         <td><?= $row['nim']; ?></td>
-         <td><?= $row['nama']; ?></td>
-         <td><?= $row['prodi']; ?></td>
-         <td><?= $row['semester']; ?></td>
-         <td>
-           <a href="edit.php?id=<?= $row['id']; ?>">Edit</a>
-           <a href="delete.php?id=<?= $row['id']; ?>" onclick="return confirm('Yakin Ingin Menghapus Data Ini?')">Hapus</a>
-         </td>
-       </tr>
-     <?php endwhile; ?>
-   </table>
- </body>
+        <tr>
+          <td style="text-align: center;"><?= $no++; ?></td>
+          <td style="text-align: center;"><?= $row['nim']; ?></td>
+          <td style="text-align: center;"><?= $row['nama']; ?></td>
+          <td style="text-align: center;"><?= $row['prodi']; ?></td>
+          <td style="text-align: center;"><?= $row['semester']; ?></td>
+          <td style="text-align: center;">
+            <a href="edit.php?id=<?= $row['id']; ?>" class="btn-edit">Edit</a>
+            <a href="delete.php?id=<?= $row['id']; ?>" class="btn-delete" onclick="return confirm('Yakin Ingin Menghapus Data Ini?')">Hapus</a>
+          </td>
+        </tr>
+      <?php endwhile; ?>
+    </tbody>
+  </table>
+</body>
 
- </html>
+</html>
